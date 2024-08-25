c9391fa2e47f27e30a5dea45f2ab2890bd8c004d
Егор Мутин <mutin.egor@gmail.com>
2024-08-25 10:22:27 +0300

    Добавьте файлы проекта.

---
+++ Задание 3/Project3.cs
@@ -0,0 +1,34 @@
+﻿public class Project3
+{
+    public void Run()
+    {
+
+
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool divisor = false;
+        int i = 2;
+
+        while (i < number)
+        {
+            if (number % i == 0)
+            {
+                divisor = true;
+                break;
+            }
+            i++;
+        }
+
+        if (!divisor && number > 1)
+        {
+            Console.WriteLine($"Число {number} является простым числом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не является простым числом");
+        }
+
+        Console.ReadKey();
+    }
+}
